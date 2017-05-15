1f2039642850bb4ff6489267f5b7ddf51d19f281
linuxitux <linuxito@openmailbox.org>
2017-05-15 09:58:06 -0400

    Changed config.php to sample

---
+++ config.example.php
@@ -0,0 +1,18 @@
+<?php
+
+/**** Configuration ****/
+
+// User
+$user = "admin";
+// Passsword for user
+$pass = "poipoipoi";
+// Hostname
+$hostname = exec("hostname -A");
+// Network device
+$iface = "venet0";
+// Samples for each run, 1 per second
+$samples = 1;
+// Update interval, must be > $samples
+$update_interval = 3;
+
+/***********************/
